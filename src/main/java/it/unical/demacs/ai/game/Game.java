e2a6d30be99d4fd71e2a39d6476ef87cd3d00d2d
SalvatoreBia <salvatorebia02@gmail.com>
2024-04-07 12:02:58 +0200

    push del canPlaceWall (forse giusto)

--- src/main/java/it/unical/demacs/ai/game/Game.java
+++ src/main/java/it/unical/demacs/ai/game/Game.java
@@ -6,10 +6,12 @@ public class Game
 {
     private static Game game = new Game();
     private Map<Integer, Pair<Wall, Wall>> walls;
+    private int wallId;
 
     private Game()
     {
         walls = new HashMap<>();
+        wallId = 1;
     }
     
     public static Game getInstance() { return game; }
@@ -31,4 +33,40 @@ public class Game
         }
         return true;
     }
+
+    public boolean canPlaceWall(Pair<Wall, Wall> toPlace)
+    {
+        for (Integer key : walls.keySet())
+            if (walls.get(key).equals(toPlace))
+                return false;
+
+        for (Integer key : walls.keySet())
+        {
+            Pair<Wall, Wall> temp = walls.get(key);
+            if ((toPlace.first.equals(new Wall(temp.first.getCellA(), temp.second.getCellA())) && 
+                toPlace.second.equals(new Wall(temp.first.getCellB(), temp.second.getCellB()))) || 
+                (toPlace.second.equals(new Wall(temp.first.getCellA(), temp.second.getCellA())) && 
+                toPlace.first.equals(new Wall(temp.first.getCellB(), temp.second.getCellB()))))
+
+                return false;
+        }
+        return true;
+    }
+
+
+    public void placeWall(Pair<Wall, Wall> toPlace)
+    {
+        walls.put(wallId, toPlace);
+        wallId++;
+    }
+
+    public static void main(String [] args){
+        Wall wall1 = new Wall(new Pair<Integer,Integer>(1, 1), new Pair<Integer,Integer>(1, 2));
+        Wall wall2 = new Wall(new Pair<Integer,Integer>(2, 1), new Pair<Integer,Integer>(2, 2));
+        Wall wall3 = new Wall(new Pair<Integer,Integer>(1, 1), new Pair<Integer,Integer>(2, 1));
+        Wall wall4 = new Wall(new Pair<Integer,Integer>(1, 2), new Pair<Integer,Integer>(2, 2));
+        Game.getInstance().placeWall(new Pair<>(wall3, wall4));
+        System.out.println(Game.getInstance().canPlaceWall(new Pair<>(wall2, wall1)));
+    }
+    
 }
