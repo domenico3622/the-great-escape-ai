a981cb81a7c3964c4ab31a5ba13bbf8360a9707a
Davide Pirrò <39238291+dpunv@users.noreply.github.com>
2024-04-18 17:12:20 +0200

    aggiunti file Agent.java

---
+++ src/main/java/it/unical/demacs/ai/model/ai/ASPetta_e_infera/Agent1.java
@@ -0,0 +1,18 @@
+package it.unical.demacs.ai.model.ai.ASPetta_e_infera;
+
+import it.unical.demacs.ai.model.ai.Agent;
+
+public class Agent1 implements Agent {
+
+    @Override
+    public void act() {
+    }
+
+    @Override
+    public String getName() {
+        return "ASPetta & Infera";
+    }
+
+    
+    
+}
---
+++ src/main/java/it/unical/demacs/ai/model/ai/Agent.java
@@ -0,0 +1,6 @@
+package it.unical.demacs.ai.model.ai;
+
+public interface Agent {
+    public void act();
+    public String getName();
+}
